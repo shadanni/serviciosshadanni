f86c00afce0c84234f56ca2405a9650337c9564c
shadanni <shadannilissette@gmail.com>
2020-03-06 00:32:07 -0600

    commit

--- functions/index.js
+++ functions/index.js
@@ -1,8 +1,30 @@
+// Este texto se ejecuta en el servidor.
+/** Se importa el objeto functions de la librería "firebase-functions". */
 const functions = require('firebase-functions');
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//  response.send("Hello from Firebase!");
-// });
+/* Se exporta la función sobre https llamada "saludo". */
+exports.saludo = functions.https.onRequest(
+  /** Código para la función saludo.
+   * @param {{query:Object}} request solicitud que recibe el servidor.
+   * Corresponde a la librería Express.
+   * @param {{send:(texto:string)=>void}} response respuesta que devuelve el
+   * servidor. Corresponde a la librería Express. */
+  (request, response) => {
+    try {
+      // verifica que el parámetro nombre1 recibido del navegador esté correcto.
+      if (!request.query.nombre1) {
+        // Entra aquí si el nombre1 es null, undefined o ""
+        throw new Error("Falta el nombre del estado");
+      } else if (!request.query.nombre2) {
+        // Entra aquí si el nombre2 es null, undefined o ""
+        throw new Error("Falta la descripcion del estado ");
+      }
+      /* Solo se llega a esta parte si nombre1 y nombre 2 tienen un texto.
+       * Devuelve un saludo. */
+      response.send(
+        `El estado ${request.query.nombre1} tiene los lugares ${request.query.nombre2}`);
+    } catch (e) {
+      // Devuelve un texto de error.
+      response.send(e.message);
+    }
+  });
