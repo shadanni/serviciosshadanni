13ead8e31c57b74580ef48d44864cb56d722d960
shadanni <shadannilissette@gmail.com>
2020-03-07 02:03:40 -0600

    commit

--- functions/index.js
+++ functions/index.js
@@ -3,7 +3,7 @@
 const functions = require('firebase-functions');
 
 /* Se exporta la función sobre https llamada "reco". */
-exports.saludo = functions.https.onRequest(
+exports.ciudad = functions.https.onRequest(
   /** Código para la función saludo.
    * @param {{query:Object}} request solicitud que recibe el servidor.
    * Corresponde a la librería Express.
@@ -11,12 +11,8 @@ exports.saludo = functions.https.onRequest(
    * servidor. Corresponde a la librería Express. */
   (request, response) => {
     try {
-        
-      // verifica que el parámetro nombre1 recibido del navegador esté correcto.
-      if (!request.query.visita) {
-        // Entra aquí si el nombre1 es null, undefined o ""
-        throw new Error("Falta seleccionar el estado ");
-      } else if (!request.query.edad) {
+      
+	if (!request.query.edad) {
         // Entra aquí si el nombre2 es null, undefined o ""
         throw new Error("Falta agregar la edad");
       }
@@ -24,17 +20,17 @@ exports.saludo = functions.https.onRequest(
        * Devuelve un saludo. */
 
       if (request.query.visitas == "1" && request.query.edad >= "18" && request.query.edad < 60) {
-        alert("Recomendamos que visite: º El callejon del Besoº en guanajuato para que la pase muy bien");
+        response.send("Recomendamos que visite: º El callejon del Besoº en guanajuato para que la pase muy bien");
       } 
 
       
 
       else if (request.query.visitas == "2" && request.query.edad >= 18 && request.query.edad < 60) {
-              alert("Recomendamos que visite : º San Pedro Tlaquepaque º en Jalisco para que la pase muy bien");
+              response.send("Recomendamos que visite : º San Pedro Tlaquepaque º en Jalisco para que la pase muy bien");
         
           } 
       else if (request.query.bebidas == "3" && request.query.edad >= 18 && request.query.edad < 60) {
-              alert("Recomendamos que visite : ºSanta Clara del Cobreº en Michoacan para que la pase muy bien");
+              response.send("Recomendamos que visite : ºSanta Clara del Cobreº en Michoacan para que la pase muy bien");
 
         }
 
